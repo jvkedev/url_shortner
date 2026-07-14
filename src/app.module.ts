7c10b134ecdbd5c7e07d9fe3ac618e88f189423b
JVKE <shubhambrown1@gmail.com>
2026-07-14 20:25:49 +0530

    feat: Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.getOrThrow<string>('mongodbUri'),
+        connectionFactory: (connection: Connection): Connection => {
+          const logger = new Logger('MongoDB');
+          logger.log('MongoDB Connected Successfully');
+
+          return connection;
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
